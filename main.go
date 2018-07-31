7d6a6a3fe5c35af347a1ef11a2c7e5ed88ef6751
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2018-07-31 16:26:05 +0100

    init

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+const port = ":3000"
+const username = "root"
+const password = "root"
+const database = "test"
+
+func main() {
+	conf := mysql.NewConfig()
+	conf.Net = "tcp"
+	conf.User = username
+	conf.Passwd = password
+	conf.DBName = database
+
+	db, err := sql.Open("mysql", conf.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		if _, err := db.Exec("select sleep(30);"); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, err.Error())
+			return
+		}
+
+		w.WriteHeader(http.StatusNoContent)
+	}
+
+	server := http.Server{
+		Addr:    port,
+		Handler: http.HandlerFunc(handler),
+	}
+
+	idleConnsClosed := make(chan struct{})
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt)
+		<-sigint
+
+		// We received an interrupt signal, shut down.
+		if err := server.Shutdown(context.Background()); err != nil {
+			// Error from closing listeners, or context timeout:
+			log.Printf("HTTP server Shutdown: %v", err)
+		}
+		close(idleConnsClosed)
+	}()
+
+	log.Printf("listening on %s\n", server.Addr)
+
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		// Error starting or closing listener:
+		log.Printf("HTTP server ListenAndServe: %v", err)
+	}
+
+	<-idleConnsClosed
+}
